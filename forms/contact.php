2cc39e6300d9425bae4d53afef19783330916bb7
divakar007 <47806465+divakar007@users.noreply.github.com>
2024-01-22 21:30:40 -0500

    portfolio changes-2

---
+++ forms/contact.php
@@ -0,0 +1,15 @@
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $message = $_POST["message"];
+
+    $to = "adivakararao@vt.edu";
+    $subject = "New Contact Form Submission";
+    $headers = "From: $email";
+
+    mail($to, $subject, $message, $headers);
+
+    exit();
+}
